148fbe796b0e4f8cf14d0ce1be86c8d2df8dcf69
slacktracer <slacktracer@gmail.com>
2018-06-28 18:03:57 -0300

    Something weird happened earlier...

--- docs/main.mjs
+++
@@ -1,7 +0,0 @@
-import { app } from "hyperapp";
-
-import actions from "./actions.mjs";
-import state from "./state.mjs";
-import view from "./view.mjs";
-
-app(state, actions, view, window.document.body);
--- docs/view.mjs
+++
@@ -1,8 +0,0 @@
-import { h } from "ijk";
-
-import main from "./views/main.mjs";
-
-import "./main.css";
-
-export default (state, actions) =>
-  h("nodeName", "attributes", "children")(["div", [main({ actions, state })]]);
